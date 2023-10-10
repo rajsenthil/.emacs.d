f01ee72c9a2de44344fe5e07f7b49f05cb9c1343
Senthil Rajendran <rajsenthil@yahoo.com>
2023-10-10 07:24:03 -0700

    changed java home

--- linux.el
+++ linux.el
@@ -6,6 +6,6 @@
 
 ;;(setenv "JAVA_HOME"  "path_to_java_folder/Contents/Home/")
 ;;(setq lsp-java-java-path "path_to_java_folder/Contents/Home/bin/java")
-(setenv "JAVA_HOME" "/home/senthil/java/jdk-18.0.1.1")
-(setq lsp-java-java-path "/home/senthil/java/jdk-18.0.1.1/bin")
+(setenv "JAVA_HOME" "/home/senthil/java/jdk-18.0.2")
+(setq lsp-java-java-path "/home/senthil/java/jdk-18.0.2/bin")
 
